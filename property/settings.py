2c9eab295756a61080dcd94ea488da96fac8fec3
bloodforcream <bfcream1@gmail.com>
2019-09-25 14:48:22 +0300

    procfile

--- property/settings.py
+++ property/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*ffjo6ck%7rercq5n0($s8g*1$#xcn^x1#y=i7yoon=5$(8kyu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['dream-property.herokuapp.com']
 
